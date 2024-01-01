2cc0d2cc1fde348a52ef3db8df2b0b300c9b374f
Arya <arya12345kishan@gmail.com>
2024-01-01 17:44:37 +0530

    fourth

--- index.js
+++ index.js
@@ -1,53 +1,16 @@
-const express = require('express');
-const cors = require("cors")
-const PORT = 8080
+const express = require("express")
 
-const stripe = require('stripe')('sk_test_51OTSOaSCLk89VVV2rKVOHYuhtVhatr42Idu62Nn2xa0Pr3Fsee5JL687eoWbCAkaU7DAMKXrSUkpvjmkcpuWyw2U00ZIT6Ag03');
+const obj = { name: "arya", age: 23, graduation: "BCA" }
+const server = express()
 
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/arya", (req, res) => {
-    res.json({ name: "arya", age: 23 })
+server.use("/get", (req, res) => {
+    res.send(JSON.stringify(obj))
 })
 
-app.post('/create-checkout-session', async (req, res) => {
-
-    console.log(req.body);
-
-    const lineItems = req.body.map((e) => ({
-        price_data: {
-            currency: "inr",
-            product_data: {
-                name: e.brand
-            },
-            unit_amount: e.price * 100
-        },
-        quantity: e.quantity
-    }))
-
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items: lineItems,
-        mode: "payment",
-        success_url: `http://localhost:5173/success`,
-        cancel_url: `http://localhost:5173/cancel`,
-    });
-
-    res.json({ id: session.id });
-
-
-    // res.send('POST REQUEST');
-
-
-});
-
-app.get("/", (req, res) => {
-
+server.use("/", (req, res) => {
+    res.send("arya")
 })
 
-app.listen(PORT, () => console.log('Running on port 8080'));
-
+server.listen("8080", () => {
+    console.log("server listened");
+})
